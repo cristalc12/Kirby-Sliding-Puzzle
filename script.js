ce46f8562db5cf318bdeb8ae514d4d669d92a0f2
cristalc12 <105386097+cristalc12@users.noreply.github.com>
2023-07-20 09:37:44 -0400

    Update script.js
    deletion of shuffle function that didn't work

--- script.js
+++ script.js
@@ -94,18 +94,4 @@ function dragEnd() {
     }
 
     button = document.getElementById("shuffle");
-function shuffle(items)
-{
-    var cached = items.slice(0), temp, i = cached.length, rand;
-    while(--i)
-    {
-        rand = Math.floor(i * Math.random());
-        temp = cached[rand];
-        cached[rand] = cached[i];
-        cached[i] = temp;
-    }
-    return cached;
-}
-  document.getElementById("shuffle").onclick = shuffleNodes;
-
 }
